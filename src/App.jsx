ce10c19718b7aacb06b3858bd448442b9d36787f
Akhil Mali <maliakhil942@gmail.com>
2026-01-20 17:52:00 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import TemporaryStudy from "./Pages/Migration/Study Visa/TemporaryStudy";
 import PostStudy from "./Pages/Migration/Study Visa/PostStudy";
 import SkilledIndependent from "./Pages/Migration/Permanent Visa/SkilledIndependent";
 import SkilledNominated from "./Pages/Migration/Permanent Visa/SkilledNominated";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
-      <Routes>
 
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/migration/study-visa/temporary" element={<TemporaryStudy />} />
         <Route path="/migration/study-visa/post-study" element={<PostStudy />} />
         <Route path="/migration/permanent-visa/skilled-independent" element={<SkilledIndependent />} />
         <Route path="/migration/permanent-visa/skilled-nominated" element={<SkilledNominated />} />
       </Routes>
+
       <Footer />
     </>
   );
